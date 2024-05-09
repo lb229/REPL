698be450b659fcd5d9426fd6638f04973953ac48
Lucy Lainez <156219119+lb229@users.noreply.github.com>
2024-05-09 16:23:52 +0200

    Add files via upload

---
+++ crypto.js
@@ -0,0 +1,20 @@
+
+/* Use the Node.js REPL to list the methods provided by the Node.js core crypto module. Use one of these methods to generate a random ID. */
+
+ crypto.
+
+
+crypto.__proto__             
+crypto.hasOwnProperty;       
+crypto.isPrototypeOf         
+crypto.propertyIsEnumerable
+crypto.toLocaleString        
+crypto.toString              
+crypto.valueOf
+crypto.constructor           
+crypto.getRandomValues       
+crypto.randomUUID            
+crypto.subtle
+
+> crypto.randomUUID()
+'2807f4e3-e447-4dd7-a573-8e5e36fbf6b8'
